4efb69bb97a8681a22b6a050dfe4abc585bd450c
Aythami <aypeve@gmail.com>
2025-03-19 22:12:55 +0000

    - Creacion de la zonas de interes del mapa (inacabado). - Modifcacion del map.js para su funcionamiento.

--- js/map.js
+++ js/map.js
@@ -1,32 +1,36 @@
-
 function initMap() {
-    let map = L.map('map').setView([28.299, -16.413], 8);
+    let map = L.map('map', {
+        center: [28.299, -16.413],
+        zoom: 8,
+        maxBounds: [
+            [27.5, -18.5], // Suroeste de Canarias
+            [29.5, -13.0]  // Noreste de Canarias
+        ],
+        maxBoundsViscosity: 1.0
+    });
+
+    // Agregar capa base de OpenStreetMap
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-
-    function addMarkersToMap(zonas) {
-        for (let lugar in zonas) {
-            let lat = zonas[lugar][0];
-            let lon = zonas[lugar][1];
-            L.marker([lat, lon]).addTo(map)
-                .bindPopup('<b>' + lugar + '</b>');
-        }
-    }
-
-
-    fetch('../zonas.json')
+    // Cargar y mostrar polígonos desde un archivo GeoJSON
+    fetch('../Data/zonas_litoral.json') // Asegúrate de que la ruta del archivo sea correcta
         .then(response => response.json())
-        .then(zonas => {
-            addMarkersToMap(zonas);
+        .then(geojsonData => {
+            L.geoJSON(geojsonData, {
+                style: feature => ({
+                    color: feature.properties.color || "blue", // Usa el color definido en GeoJSON o azul por defecto
+                    weight: 2,
+                    opacity: 0.8,
+                    fillOpacity: 0.4
+                }),
+                onEachFeature: (feature, layer) => {
+                    if (feature.properties && feature.properties.name) {
+                        layer.bindPopup(`<b>${feature.properties.name}</b>`);
+                    }
+                }
+            }).addTo(map);
         })
-        .catch(error => console.error('Error al cargar el archivo JSON:', error));
-
-    fetch('../beaches.json')
-        .then(response => response.json())
-        .then(beaches => {
-
-        })
+        .catch(error => console.error('Error al cargar el archivo zonas_litoral.json:', error));
 }
-
