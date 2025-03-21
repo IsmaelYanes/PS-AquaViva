4c86ff064e2287021b9a54c152be08aa18a586d4
Aythami <aypeve@gmail.com>
2025-03-21 18:23:12 +0000

    - Refactorizacion del codigo para mejorar su estructura. - Ajustes del mapa y separacion del html y script

--- src/JS/map.js
+++ src/JS/map.js
@@ -1,7 +1,8 @@
-function initMap() {
+document.addEventListener("DOMContentLoaded", function() {
     let map = L.map('map', {
         center: [28.299, -16.413],
         zoom: 8,
+        minZoom: 8, // Restringe el zoom para que no pueda alejarse más allá de 8
         maxBounds: [
             [27.5, -18.5], // Suroeste de Canarias
             [29.5, -13.0]  // Noreste de Canarias
@@ -20,7 +21,7 @@ function initMap() {
         .then(geojsonData => {
             L.geoJSON(geojsonData, {
                 style: feature => ({
-                    color: feature.properties.color || "blue", // Usa el color definido en GeoJSON o azul por defecto
+                    color: feature.properties.color || "blue",
                     weight: 2,
                     opacity: 0.8,
                     fillOpacity: 0.4
@@ -33,4 +34,4 @@ function initMap() {
             }).addTo(map);
         })
         .catch(error => console.error('Error al cargar el archivo zonas_litoral.json:', error));
-}
+});
