e57af8d57ffe2492dd64a438acd0c42d72e29551
Ismael <147845869+IsmaelYanes@users.noreply.github.com>
2025-03-21 17:34:41 +0000

    Update ToolbarToggle.js

--- Scripts/ToolbarToggle.js
+++ Scripts/ToolbarToggle.js
@@ -1,9 +1,12 @@
 function toggleToolbar() {
+    let toolbar = document.getElementById("toolbar");
     let content = document.getElementById("toolbar-content");
 
+    toolbar.classList.toggle("expanded");
+
     if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "flex"; // Mostrar en columna
+        content.style.display = "flex";
     } else {
-        content.style.display = "none"; // Ocultar
+        content.style.display = "none";
     }
 }
