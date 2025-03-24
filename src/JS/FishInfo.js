7410f1afaa71c0a1d86d420723351ec5ab142e40
Ismael <fyqismaelperezyanes@gmail.com>
2025-03-24 16:54:41 +0000

    bibliografía de peces //TODO

---
+++ src/JS/FishInfo.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../Data/fish.json')
+        .then(response => response.json())
+        .then(data => {
+            const fishGrid = document.getElementById('fish-grid');
+
+            data.forEach(fish => {
+                const fishItem = document.createElement('div');
+                fishItem.classList.add('fish-item');
+
+                const fishLink = document.createElement('a');
+                fishLink.href = "#"; // Puedes poner el enlace de cada pez aquí.
+
+                const fishImage = document.createElement('img');
+                fishImage.src = fish.image;
+                fishImage.alt = fish.name;
+
+                const fishName = document.createElement('h2');
+                fishName.textContent = fish.name;
+
+                fishLink.appendChild(fishImage);
+                fishLink.appendChild(fishName);
+                fishItem.appendChild(fishLink);
+                fishGrid.appendChild(fishItem);
+            });
+        })
+        .catch(error => console.error('Error al cargar los peces:', error));
+});
