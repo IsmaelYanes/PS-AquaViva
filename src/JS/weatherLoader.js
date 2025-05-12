70f87923b4b64e5ac4dd8cbd0600560d9d531420
crazy <crazydoufuz@gmail.com>
2025-05-12 16:07:53 +0100

    add weather notify function

---
+++ src/JS/weatherLoader.js
@@ -0,0 +1,42 @@
+class WeatherControl {
+    constructor() {
+        this.temperature = 0;
+        this.windSpeed = 0;
+        this.precipitation = 0;
+        this.TEMPERATUREUMBRAL = 2;
+        this.WINDSPEEDUMBRAL = 15;
+        this.PRECIPITATIONUMBRAL = 5;
+
+    }
+
+    async loadWeather(lat, lon) {
+        const response = await fetch(`https://api.weatherapi.com/v1/marine.json?key=${KEY}&q=${lat},${lon}`);
+        const data = await response.json();
+        this.temperature = data.current.temp_c;
+        this.windSpeed = data.condition.wind_kph;
+        this.precipitation = data.condition.precip_mm;
+    }
+
+    notifyAlert(){
+
+    }
+    updateTemperature(temperature) {
+        if (Math.abs(temperature - this.temperature) > this.TEMPERATUREUMBRAL) {
+            this.notifyAlert();
+        }
+        this.temperature = temperature;
+    };
+
+    updatePrecipitation(precipitation) {
+        if ( (precipitation - this.precipitation) > this.PRECIPITATIONUMBRAL ) {
+            this.notifyAlert();
+        }
+        this.precipitation = precipitation;
+    }
+
+    updateWindSpeed(windSpeed) {
+        if ( (windSpeed - this.windSpeed) > this.WINDSPEEDUMBRAL ) {
+            this.notifyAlert();
+        }
+    }
+}
